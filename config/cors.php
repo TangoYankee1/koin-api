455d81999219642ba341d1c2a550b52c59749953
TangoYankee1 <aicsjohnrecanil@gmail.com>
2025-12-16 19:37:35 +0800

    modified:   config/cors.php

--- config/cors.php
+++ config/cors.php
@@ -5,11 +5,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:5000',
-        'http://localhost:5173',
-        'http://localhost:3000',
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_headers' => ['*'],
 
