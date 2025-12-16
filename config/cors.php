573109b393a11f8c83b1980fcd96e90b04b28429
TangoYankee1 <aicsjohnrecanil@gmail.com>
2025-12-16 17:04:57 +0800

    modified:   bootstrap/app.php 	modified:   config/cors.php

--- config/cors.php
+++ config/cors.php
@@ -2,28 +2,14 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://koin-web-psi.vercel.app',
-        'http://localhost:5000',
-        'https://koin-d484kv2a2-john-recanils-projects.vercel.app',
-        'https://koin-api.onrender.com',
+        'http://localhost:5173',
+        'http://localhost:3000',
+        'https://koin-d484kv2a2-john-recanils-projects.vercel.app'
     ],
 
     'allowed_origins_patterns' => [],
@@ -34,6 +20,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
