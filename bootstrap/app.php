0708e3633be02e5e537234ce6d6ddc81af6152bf
TangoYankee1 <aicsjohnrecanil@gmail.com>
2025-12-16 17:18:12 +0800

    modified:   bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
 
-        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+        $middleware->append(HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
